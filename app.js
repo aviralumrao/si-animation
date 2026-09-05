be02acabb135a6c0f0a330ce12daf1b5b8f7eba3
aviralumrao <aviral0419@gmail.com>
2026-09-05 20:12:43 +0530

    added a space shuttle and reset funtion

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const shuttleImg = new Image();
+shuttleImg.src = "assets/spaceShuttle.png";
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(50, 50, 100, 100);
+const shuttle = {
+  x: 220,
+  y: 340,
+  width: 60,
+  height: 60,
+  speed: 6
+};
+
+function drawShuttle() {
+  ctx.drawImage(shuttleImg, shuttle.x, shuttle.y, shuttle.width, shuttle.height);
+}
+
+
+
+
+//restart game
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawShuttle();
+  requestAnimationFrame(gameLoop);
+}
